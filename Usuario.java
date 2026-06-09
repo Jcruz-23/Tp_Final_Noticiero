79bd5d6f800832decfe5ae80d3e71678939531a1
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-06-08 21:22:51 -0300

    Creacion Usuario

---
+++ Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    private Integer dni;
+    private String nombre;
+    private Integer edad;
+
+    Usuario(Integer dni, String nombre, Integer edad){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}

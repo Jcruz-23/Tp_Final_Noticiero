dede79198f50a1f1a757b8f90f2adeebff24237c
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-06-10 18:14:48 -0300

    Excepciones fecha

--- Fecha.java
+++ Fecha.java
@@ -4,6 +4,12 @@ public class Fecha{
     private Integer anio;
 
     public Fecha(Integer dia, Integer mes, Integer anio){
+        if(dia == null) throw new NullPointerException("dia no puede estar vacio");
+        if(mes == null) throw new NullPointerException("mes no puede estar vacio");
+        if(anio == null) throw new NullPointerException("anio no puede estar vacio");
+        if(dia < 1 || dia > 31) throw new IllegalArgumentException("dia debe estar en el dominio [1,31]");
+        if(mes < 1 || dia > 12) throw new IllegalArgumentException("dia debe estar en el dominio [1,12]");
+        if(dia < 2010 || dia > 2030) throw new IllegalArgumentException("dia debe estar en el dominio [2010,2030]");
         this.dia = dia;
         this.mes = mes;
         this. anio = anio;

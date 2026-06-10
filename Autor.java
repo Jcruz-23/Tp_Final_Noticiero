6be2462659c7d520f411a21e5ab0d0081142571a
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-06-10 18:01:56 -0300

    Excepcion de autor

--- Autor.java
+++ Autor.java
@@ -1,8 +1,9 @@
 public class Autor extends Usuario{
     private String medio;
 
-    Autor(Integer dni, String nombre, Integer edad, String medio){
+    public Autor(Integer dni, String nombre, Integer edad, String medio){
         super(dni, nombre, edad);
+        if(medio == null || medio.isEmpty()) throw new NullPointerException("medio no puede estar vacio");
         this.medio = medio;
     }
 

a3160f32ca590f8bc59b6c9d04f289dc1ebe5277
Jcruz-23 <jsanchez041@alumnos.iua.edu.ar>
2026-06-10 17:57:06 -0300

    Excepcion de Usuario

--- Usuario.java
+++ Usuario.java
@@ -4,6 +4,11 @@ public class Usuario {
     private Integer edad;
 
     Usuario(Integer dni, String nombre, Integer edad){
+        if(dni == null) throw new NullPointerException("dni no puede estar vacio");
+        if(dni < 1000000 || dni > 99999999) throw new IllegalArgumentException("dni debe estar en el dominio [1000000, 99999999]");
+        if(nombre == null || nombre.isEmpty()) throw new NullPointerException("nombre no puede estar vacio");
+        if(edad == null) throw new NullPointerException("edad no puede estar vacio");
+        if(edad > 100) throw new IllegalArgumentException("Edad no puede ser mayor de 100");
         this.dni = dni;
         this.nombre = nombre;
         this.edad = edad;

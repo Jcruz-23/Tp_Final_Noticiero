5ad7d40de4073a9a81bcdb240e1c93f38c408b91
rgarzon846 <rgarzon846@alumnos.iua.edu.ar>
2026-06-14 11:13:19 -0300

    Control de rangos

--- Fecha.java
+++ Fecha.java
@@ -8,8 +8,8 @@ public class Fecha{
         if(mes == null) throw new NullPointerException("mes no puede estar vacio");
         if(anio == null) throw new NullPointerException("anio no puede estar vacio");
         if(dia < 1 || dia > 31) throw new IllegalArgumentException("dia debe estar en el dominio [1,31]");
-        if(mes < 1 || dia > 12) throw new IllegalArgumentException("dia debe estar en el dominio [1,12]");
-        if(dia < 2010 || dia > 2030) throw new IllegalArgumentException("dia debe estar en el dominio [2010,2030]");
+        if(mes < 1 || dia > 12) throw new IllegalArgumentException("mes debe estar en el dominio [1,12]");
+        if(dia < 2010 || dia > 2030) throw new IllegalArgumentException("año debe estar en el dominio [2026,2050]");
         this.dia = dia;
         this.mes = mes;
         this. anio = anio;
